6954d453cf39a3e8a40ab63fadf46ca68a2275bc
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-09-21 19:17:59 -0400

    Record differences in weight

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -10,9 +10,12 @@ const HistoryList = (props) => {
 
     const entryList = () => {
 
-        let date = '', day = '', month = '', year = '', formattedDate = '', weight = '';
+        console.log(props.entries);
 
-        const entryHTML = props.entries.map((item) => {
+        let date = '', day = '', month = '', year = '', deltaChange = 'caret-up',
+            formattedDate = '', weight = '', weightDiff = 0;
+
+        const entryHTML = props.entries.map((item, index) => {
             console.log(item);
             date = item.date;
             date = new Date(date);
@@ -21,20 +24,32 @@ const HistoryList = (props) => {
             year = (date.getFullYear()).toString().substr(-2);
 
             formattedDate = day + '/' + month + '/' + year;
-            weight = item.weight[`${props.kgOrIb}`] + props.kgOrIb
-            
+            weight = item.weight[`${props.kgOrIb}`];
+
+            // Calculate difference in weight between subsequent records
+            if (index < props.entries.length - 1) {    // Avoid throwing error for last entry
+                weightDiff = Number(props.entries[index].weight[`${props.kgOrIb}`]) - Number(props.entries[index + 1].weight[`${props.kgOrIb}`]);
+                weightDiff = weightDiff.toFixed(1);
+                
+                deltaChange = (weightDiff.indexOf('-') ? 'caret-up' : 'caret-down');
+
+                weightDiff = weightDiff.replace('-', '') + props.kgOrIb;  // 2.0kg
+            } else {
+                weightDiff = '';    // Override with empty value for last record
+            }
+            // console.log(props.entries[index].weight[`${props.kgOrIb}`], props.entries[index + 1].weight[`${props.kgOrIb}`])
 
             return (
-                <li key={item} className="">
+                <li key={index} className="">
                     <span className="">
                         {formattedDate}
                     </span>
                     <span className="">
-                        {weight}
+                        {weight + props.kgOrIb}
                     </span>
                     <strong className="">
-                        <span><FontAwesome name="caret-up" /> </span>
-                        <span className="">2.0</span>Ib
+                        {(weightDiff === '') ? '' : <span><FontAwesome name={deltaChange} /></span> }
+                        <span className="">{weightDiff}</span>
                     </strong>
                     <span className={styles.amend_entry}>
                         <button id="edit">
