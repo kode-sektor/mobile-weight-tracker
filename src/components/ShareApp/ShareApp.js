79a149a116a85ce493ac45521a560c45f180b89b
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-09-24 02:05:25 -0400

    Complete sharing button styling

--- src/components/ShareApp/ShareApp.js
+++ src/components/ShareApp/ShareApp.js
@@ -1,30 +1,29 @@
 import React from 'react';
 
-import {Link} from 'react-router-dom';
-
 // Parent file: home.js
+
+import styles from './ShareApp.module.css';
 
 const RateUs = (props) => {
 
     // NB 👉: Fake phone no below
 
 	return (
-        <section className={` ${props.showShareApp} module`}>
+        <section className={`${props.showShareApp} module`}>
             
-            <ul className="">
-
+            <ul className={styles.sharePlatforms}>
                 <li>
-                    <iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.minstermedia.android.weighttracker%26hl%3Den&layout=button&size=large&width=76&height=28&appId" 
-                        width="76" height="28" style={{border: 'none', overflow: 'hidden'}}
+                    <iframe className={`${styles.facebook} share-fb`} src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.minstermedia.android.weighttracker%26hl%3Den&layout=button&size=large&width=76&height=28&appId" 
+                        width="100%" height="28" style={{border: 'none', overflow: 'hidden'}}
                         scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media">
                     </iframe>
                 </li>
-                <li>
+                <li className={styles.whatsapp}>
                     <a href={`https://api.whatsapp.com/send?phone=234-XXXXXXXXXXtext=%20${props.socials.whatsapp}`}
-                        width="76" height="28" rel="external" target="_blank">Whatsapp
+                        rel="external" target="_blank">Whatsapp
                     </a>
                 </li>
-                <li>
+                <li className={styles.twitter}>
                     <a class="twitter-share-button" data-size="large" rel="external" target="_blank" href={`https://twitter.com/intent/tweet?text=${props.socials.twitter}`}>
                         Tweet
                     </a>
